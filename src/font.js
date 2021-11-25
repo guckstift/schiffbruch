a6450919d01ea545e1c6a1a54f5b322c55cb87c6
guckstift <guckstift@posteo.de>
2021-11-26 00:30:11 +0100

    first commit

---
+++ src/font.js
@@ -0,0 +1,14 @@
+const font1_w = 20;
+const font1_h = 20;
+const font1_pitch = 13;
+
+const font2_w = 10;
+const font2_h = 15;
+const font2_pitch = 10;
+
+class Font
+{
+	constructor()
+	{
+	}
+}
---
+++ src/map.js
@@ -0,0 +1,12 @@
+const tile_w = 54;
+const tile_h = 44;
+
+const map_w = 60;
+const map_h = 60;
+
+class Map
+{
+	constructor()
+	{
+	}
+}
